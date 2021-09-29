c7da270fd77c51f3d43907a66066913bca56717f
amaso7 <saadhindash@gmail.com>
2021-09-29 17:43:59 -0400

    added sql DB, server,and buttons

---
+++ client/README.md
@@ -0,0 +1,6 @@
+## Week 13 Individual project Digital crafts Full-Stack Cohort
+_______________________________________________________________
+
+**Dr. / Patient Portal App**
+_______________________________________________________________
+*Developed by Saad Hindash*
---
+++ client/src/DrListPage.js
@@ -0,0 +1,36 @@
+
+import './App.css';
+import {useEffect, useState } from 'react'
+
+function DrListPage(props) {
+
+  const [drs, setDrs] = useState([])
+  useEffect(() => {
+    fetch('http://localhost:5000/api/drs')
+    .then(response => response.json())
+    .then(drs => {
+      setDrs(drs)
+    })
+  }, [])
+
+  const drItems = drs.map(dr =>{
+    return <li key = {dr.id}>{dr.drName}</li>
+  })
+
+  return (
+    <div className="App">
+      <header className="App-header">
+        <p>Azure for Health and Human Services LLC. Dr./Patient Portal</p>
+        <img src="https://findicons.com/files/icons/2837/health/128/doctor_icon.png"/>        
+        <button className = "buttonpt">Patient Portal</button>
+        <button className = "buttondr">Doctor Login</button>
+      </header>
+
+      <ul> 
+        {drItems}
+      </ul>
+    </div>
+  )
+}
+
+export default DrListPage;
